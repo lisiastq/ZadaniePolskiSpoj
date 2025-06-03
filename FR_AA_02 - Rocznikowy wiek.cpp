cfa6f3faa5172a8331ebf8fd2e541ec7ce224213
Lisiasty <kiciola57@gmail.com>
2025-06-03 21:53:11 +0200

    Create FR_AA_02 - Rocznikowy wiek.cpp

---
+++ FR_AA_02 - Rocznikowy wiek.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	int ulu,rok,os;
+	cin>>ulu>>rok;
+	os%=10;
+	os*=2;
+	os+=5;
+	os*=50;
+	os+=1771;
+	os-=rok;
+	cout<<os;
+} 
