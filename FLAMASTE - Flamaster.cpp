ed96dbd5064e4a7853e7c29d270eecda786cba45
Lisiasty <kiciola57@gmail.com>
2025-06-03 21:45:47 +0200

    Create FLAMASTE - Flamaster.cpp

---
+++ FLAMASTE - Flamaster.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	
+	    int ile;
+    cin>>ile;
+    while(ile--)
+    {
+        string napis;
+        cin>>napis;
+        int licznik=1;
+        for (int i=1; i<=napis.length(); i++)
+        {
+            if(napis[i] != napis[i-1])
+            {
+                if(licznik > 2)
+                    cout<< napis[i-1] << licznik;
+                else if (licznik == 2)
+                    cout<< napis[i-1] << napis[i-1];
+                else
+                    cout<<napis[i-1];
+                licznik=1;
+            }
+            else
+                licznik++;
+        }
+    cout<<"\n";
+    }
+    return 0;
+} 
