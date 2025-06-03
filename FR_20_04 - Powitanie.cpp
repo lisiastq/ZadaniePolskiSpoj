174f08da57533a5b9660a5950388f33c0c09d7be
Lisiasty <kiciola57@gmail.com>
2025-06-03 21:54:12 +0200

    Create FR_20_04 - Powitanie.cpp

---
+++ FR_20_04 - Powitanie.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a,b;
+	cin>>a>>b;
+	cout<<"Witaj na V Konkursie FRAKTAL! Suma "<<a<<" i "<<b<<" to "<<a+b<<"."<<endl<<"Powodzenia, programisto!";
+}  
