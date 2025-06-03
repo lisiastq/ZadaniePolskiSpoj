4d04b9dc1e0c2367a165717e5740b3697250e615
Lisiasty <kiciola57@gmail.com>
2025-06-03 21:47:49 +0200

    Create  FR_16_01 - Ropuszka.cpp

---
+++ FR_16_01 - Ropuszka.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c;
+	cin>>a>>b>>c;
+	if(c==33){
+		cout<<"WRACAJ";
+	}
+	else if(a>b){
+		cout<<"W PRAWO";
+	}
+	else if(a<b){
+		cout<<"W LEWO";
+	}
+}
+ 
